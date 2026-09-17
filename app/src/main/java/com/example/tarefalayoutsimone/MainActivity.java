e2b93d18d515057c28d7a9353a2ea9c9046e571a
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2026-09-17 11:18:13 -0300

    feat: Criando o MainActivity

--- app/src/main/java/com/example/tarefalayoutsimone/MainActivity.java
+++ app/src/main/java/com/example/tarefalayoutsimone/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.tarefalayoutsimone;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import com.google.android.material.button.MaterialButton;
 public class MainActivity extends AppCompatActivity {
-
+    private MaterialButton btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        inicializarComponentes();
+        configurarEventos();
+    }
+    private void inicializarComponentes() {
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+    private void configurarEventos() {
+        btnLogin.setOnClickListener(v ->
+                Toast.makeText(this,
+                        "Login realizado com sucesso!",
+                        Toast.LENGTH_SHORT).show()
+        );
     }
 }
